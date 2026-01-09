8a7863336d758899bc393b56794e05c1589e114b
Simranjeet kaur <sunil.salaria@lnpinfotech.com>
2026-01-09 19:29:55 +0530

    Question test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Carousel from "../components/Carousel";
 import ShopBy from "../components/ShopBy";
 import GenInfo, { Brands } from "../components/GenInfo";
@@ -13,6 +12,7 @@ const Home = () => {
         <div className="prose prose-2xl">
           <ShopBy title="Best Sellers" filter="bestSellers" />
         </div>
+        <Brands showTitle={false} />
         <div className="prose prose-2xl">
           <ShopBy title="Top Rated" filter="topRated" />
         </div>
